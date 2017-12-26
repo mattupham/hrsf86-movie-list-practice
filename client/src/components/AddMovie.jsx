5ef0df53051ac7a9c05cf4ae4cbf7e4f94bbf2d3
Hack Reactor Student <student@hackreactor.com>
2017-12-26 15:21:39 -0800

    complete basic state change

--- client/src/components/AddMovie.jsx
+++ client/src/components/AddMovie.jsx
@@ -1,24 +1,49 @@
 import React from 'react';
 
-let AddMovie = (props) => {
-  return (
-    <div className="addMovieListItemContainer">
+class AddMovie extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {value: ''}
     
-        <input 
-          className="addMovieInput" 
-          type="text" 
-          placeholder="Add movie title here"
-        ></input>
-        
-        <input 
-          className="addMovieButton" 
-          type="submit" 
-          onClick={props.handleAddMovieClick}
-          value="Add"
-        ></input>
-        
-    </div>
-  )
+    this.handleAddMovieInputChange = this.handleAddMovieInputChange.bind(this);
+    this.handleAddMovieSubmit = this.handleAddMovieSubmit.bind(this);
+    
+  }
+  
+  handleAddMovieInputChange(event){
+    this.setState({value: event.target.value});
+    console.log(this.state.value);
+  }
+  
+  handleAddMovieSubmit(event){
+    alert(this.state.value);
+    event.preventDefault();
+  }
+  
+  render() {
+    return (
+      <form 
+        className="addMovieListItemContainer"
+        onSubmit={this.handleAddMovieSubmit}
+      >
+          <input 
+            className="addMovieInput" 
+            type="text" 
+            // placeholder="Add movie title here"
+            value={this.state.value}
+            onChange={this.handleAddMovieInputChange}
+          ></input>
+          
+          <input 
+            className="addMovieButton" 
+            type="submit" 
+            onClick={this.props.handleAddMovieClick}
+            value="Add"
+          ></input>
+          
+      </form>
+    )
+  }
 }
 
 export default AddMovie;
