cfdad466a729bcabf248c2f6cc6a96b1e82403de
Hack Reactor Student <student@hackreactor.com>
2017-12-26 19:20:34 -0800

    complete addition of movies

--- client/src/components/AddMovie.jsx
+++ client/src/components/AddMovie.jsx
@@ -3,41 +3,41 @@ import React from 'react';
 class AddMovie extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {value: ''}
+    //this.state = {value: ''}
     
-    this.handleAddMovieInputChange = this.handleAddMovieInputChange.bind(this);
-    this.handleAddMovieSubmit = this.handleAddMovieSubmit.bind(this);
+    // this.handleAddMovieInputChange = this.handleAddMovieInputChange.bind(this);
+    // this.handleAddMovieSubmit = this.handleAddMovieSubmit.bind(this);
     
   }
   
-  handleAddMovieInputChange(event){
-    this.setState({value: event.target.value});
-    console.log(this.state.value);
-  }
+  // handleAddMovieInputChange(event){
+  //   this.setState({value: event.target.value});
+  //   console.log(this.state.value);
+  // }
   
-  handleAddMovieSubmit(event){
-    alert(this.state.value);
-    event.preventDefault();
-  }
+  // handleAddMovieSubmit(event){
+  //   alert(this.state.value);
+  //   event.preventDefault();
+  // }
   
   render() {
     return (
       <form 
         className="addMovieListItemContainer"
-        onSubmit={this.handleAddMovieSubmit}
+        onSubmit={this.props.handleAddMovieSubmit}
       >
           <input 
             className="addMovieInput" 
             type="text" 
             // placeholder="Add movie title here"
-            value={this.state.value}
-            onChange={this.handleAddMovieInputChange}
+            value={this.props.value}
+            onChange={this.props.handleAddMovieInputChange}
           ></input>
           
           <input 
             className="addMovieButton" 
             type="submit" 
-            onClick={this.props.handleAddMovieClick}
+            //onClick={this.props.handleAddMovieClick}
             value="Add"
           ></input>
           
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -8,27 +8,37 @@ import AddMovie from './components/AddMovie.jsx'
 class MovieList extends React.Component {
   constructor(props) {
     super(props);
+    // this.handleAddMovieClick = this.handleAddMovieClick.bind(this);
+    this.handleAddMovieInputChange = this.handleAddMovieInputChange.bind(this);
+    this.handleAddMovieSubmit = this.handleAddMovieSubmit.bind(this);
     this.state = {
-      movieList: [
-                  {title: 'Mean Girls'},
-                  {title: 'Hackers'},
-                  {title: 'The Grey'},
-                  {title: 'Sunshine'},
-                  {title: 'Ex Machina'},
-                ]
+      movieList: [{title: 'Mean Girls'},{title: 'Hackers'},{title: 'The Grey'},{title: 'Sunshine'},{title: 'Ex Machina'}],
+      addMovieValue: ''
     }
-    this.handleAddMovieClick = this.handleAddMovieClick.bind(this);
   }
-
-  handleAddMovieClick(event){
-    alert('clicked');
+  
+  handleAddMovieInputChange(event){
+    this.setState({addMovieValue: event.target.value});
+  }
+  
+  handleAddMovieSubmit(event){
+    //if value isn't empty, adds a movie to the list
+    if (this.state.addMovieValue !== '') {
+      this.state.movieList.push({title: this.state.addMovieValue});
+    } else {
+      alert('Please add a movie');
+    }
+    event.preventDefault();
+    this.setState({addMovieValue: ''});
   }
 
   render() {
     return (
       <div>
         <AddMovie 
-          handleAddMovieClick={this.handleAddMovieClick}
+          // handleAddMovieClick={this.handleAddMovieClick}
+          handleAddMovieInputChange={this.handleAddMovieInputChange}
+          handleAddMovieSubmit={this.handleAddMovieSubmit}
         />
         <Search />
         <MovieListContainer 
